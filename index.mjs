167b45d7316a0441d679979e02cfd7c4afaf12e9
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2022-05-18 21:22:05 +0000

    nice

--- index.mjs
+++ index.mjs
@@ -1,21 +1,37 @@
-import Server from 'bare-server-node';
-import http from 'http';
-import nodeStatic from 'node-static';
+import Server from "bare-server-node";
+import https from "https";
+// import http from "http";
+import nodeStatic from "node-static";
+import fs from "fs";
 
+const bare = new Server("/bare/", "");
+const serve = new nodeStatic.Server("static/");
 
-const bare =  new Server('/bare/', '');
-const serve = new nodeStatic.Server('static/');
+// const logFileStat = fs.statSync("logs/requests.log");
+// if (stats.size > 100000000) {
+//     console.log(`[!] log file is over 100mb, deleting log file`);
+    
+// }
 
-const server = http.createServer();
-
-server.on('request', (request, response) => {
-    if (bare.route_request(request, response)) return true;
-    serve.serve(request, response);
+const server = https.createServer({
+  key: fs.readFileSync("ssl/key.pem"),
+  cert: fs.readFileSync("ssl/cert.pem"),
+});
+// const server = http.createServer();
+server.on("request", (request, response) => {
+  console.log(
+    `[-] ${request.headers["x-bare-host"] || ""}${request.headers["x-bare-path"] || request.url}`
+  );
+console.log(request.headers);
+  if (bare.route_request(request, response)) return true;
+  serve.serve(request, response);
 });
 
-server.on('upgrade', (req, socket, head) => {
-	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
+server.on("upgrade", (req, socket, head) => {
+  if (bare.route_upgrade(req, socket, head)) return;
+  socket.end();
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen(443, () => {
+  console.log(`[-] server started on port 443`)
+});
