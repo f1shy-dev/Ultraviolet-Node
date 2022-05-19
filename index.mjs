6d56dc30e2c9b5c5eb5cfcc270539a677f9a24c4
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2022-05-19 08:33:32 +0000

    nice

--- index.mjs
+++ index.mjs
@@ -1,37 +1,37 @@
 import Server from "bare-server-node";
 import https from "https";
-// import http from "http";
+import http from "http";
 import nodeStatic from "node-static";
 import fs from "fs";
 
 const bare = new Server("/bare/", "");
 const serve = new nodeStatic.Server("static/");
 
-// const logFileStat = fs.statSync("logs/requests.log");
-// if (stats.size > 100000000) {
-//     console.log(`[!] log file is over 100mb, deleting log file`);
-    
-// }
-
-const server = https.createServer({
+const httpsServer = https.createServer({
   key: fs.readFileSync("ssl/key.pem"),
   cert: fs.readFileSync("ssl/cert.pem"),
 });
+const httpServer = http.createServer();
+
 // const server = http.createServer();
-server.on("request", (request, response) => {
+[httpServer, httpsServer].forEach(i => i.on("request", (request, response) => {
   console.log(
     `[-] ${request.headers["x-bare-host"] || ""}${request.headers["x-bare-path"] || request.url}`
   );
-console.log(request.headers);
+  console.log(request.headers);
   if (bare.route_request(request, response)) return true;
   serve.serve(request, response);
-});
+}));
 
-server.on("upgrade", (req, socket, head) => {
+[httpServer, httpsServer].forEach(i => i.on("upgrade", (req, socket, head) => {
   if (bare.route_upgrade(req, socket, head)) return;
   socket.end();
+}));
+
+httpsServer.listen(443, () => {
+  console.log(`[-] https server started on port 443`)
 });
 
-server.listen(443, () => {
-  console.log(`[-] server started on port 443`)
+httpServer.listen(8080, () => {
+  console.log(`[-] http server started on port 8080`)
 });
